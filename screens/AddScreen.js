2c75b3eda6794a19ab3ac0a2caad505eeaee9105
Boey Kok Chin <boeykokchin@live.com.sg>
2020-12-11 02:32:23 +0800

    based - task 7 OK

--- screens/AddScreen.js
+++ screens/AddScreen.js
@@ -1,16 +1,72 @@
-import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 
 export default function AddScreen({ navigation }) {
+  const [text, setText] = useState('');
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Add Screen</Text>
-      <TouchableOpacity
-        onPress={() => navigation.goBack()}
-        style={{ padding: 10 }}
-      >
-        <Text style={{ color: 'orange' }}>Dismiss</Text>
-      </TouchableOpacity>
+      <Text style={styles.label}>Add TODO</Text>
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={(newText) => setText(newText)}
+      ></TextInput>
+      <View style={styles.buttons}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={[styles.button, styles.submitButton]}
+        >
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={[styles.button, styles.cancelButton]}
+        >
+          <Text style={styles.buttonText}>Cancel</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={{ marginTop: 40, color: 'grey' }}>You typed:</Text>
+      <Text style={{ color: '#333', marginTop: 10 }}>{text}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  label: {
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+
+  textInput: {
+    margin: 20,
+    borderWidth: 1,
+    width: '80%',
+    padding: 10,
+    borderColor: '#ccc',
+  },
+
+  buttons: {
+    flexDirection: 'row',
+  },
+
+  button: {
+    padding: 10,
+    margin: 5,
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+
+  submitButton: {
+    backgroundColor: 'orange',
+  },
+
+  cancelButton: {
+    backgroundColor: 'red',
+  },
+});
