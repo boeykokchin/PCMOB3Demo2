bf61740dbf5447d77faa5583e2ce35573e704c88
Boey Kok Chin <boeykokchin@live.com.sg>
2020-12-10 15:59:09 +0800

    based - part 1 and 2 OK

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { Animated, StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import * as SQLite from 'expo-sqlite';
+
+function HomeScreen({ navigation }) {
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={addList}>
+          <MaterialCommunityIcons
+            name='circle-edit-outline'
+            size={40}
+            style={{ color: 'orange', marginRight: 20 }}
+          />
+        </TouchableOpacity>
+      ),
+    });
+  }, []);
+
+  function addList() {
+    console.log('ADD TODO LIST');
+  }
+
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerTintColor: 'orange' }}>
+        <Stack.Screen name='TODO' component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
